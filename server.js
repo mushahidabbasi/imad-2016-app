282c4639c8bf21b2bbcc20655fb7560be0985211
mushahidabbasi <zebhussain2013@gmail.com>
2016-11-09 09:47:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get('/ui/main.js', function (req, res) {
 });
 
 app.get('/ui/download.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'download.jpg'));
+  res.sendFile(path.join(__dirname, 'ui', 'blog.jpg'));
 });
 
 
