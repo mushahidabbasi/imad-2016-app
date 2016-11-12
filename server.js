4fed8163fb644c0130b23c50b83746aedbef2a89
mushahidabbasi <zebhussain2013@gmail.com>
2016-11-12 12:45:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -34,17 +34,17 @@ var articles={
 
 <form>
                 <h1>Create Logon</h1>
-                <div class="line"><label for="username">Username *: </label><input type="text" id="username" /></div>
-                <div class="line"><label for="pwd">Password *: </label><input type="password" id="pwd" /></div>
+                <div class="line"><label for="username">Username *: </label><input type="text" id="username" /></div> <br>
+                <div class="line"><label for="pwd">Password *: </label><input type="password" id="pwd" /></div> <br>
                 
-                <div class="line"><label for="surname">Surname *: </label><input type="text" id="surname" /></div>
-                <div class="line"><label for="other_names">Other Names *: </label><input type="text" id="names" /></div>
-                <div class="line"><label for="dob">Date of Birth *: </label><input type="text" id="dob" /></div>
-                <div class="line"><label for="email">Email *: </label><input type="email" id="email" /></div>
+                <div class="line"><label for="surname">Surname *: </label><input type="text" id="surname" /></div> <br>
+                <div class="line"><label for="other_names">Other Names *: </label><input type="text" id="names" /></div> <br>
+                <div class="line"><label for="dob">Date of Birth *: </label><input type="text" id="dob" /></div> <br>
+                <div class="line"><label for="email">Email *: </label><input type="email" id="email" /></div> <br>
                
-                <div class="line"><label for="tel">Telephone: </label><input type="text" id="tel" /></div>
-                <div class="line"><label for="add">Address *: </label><input type="text" id="add" /></div>
-                <div class="line"><label for="ptc">Post Code *: </label><input type="text" id="ptc" /></div>
+                <div class="line"><label for="tel">Telephone: </label><input type="text" id="tel" /></div> <br>
+                <div class="line"><label for="add">Address *: </label><input type="text" id="add" /></div> <br>
+                <div class="line"><label for="ptc">Post Code *: </label><input type="text" id="ptc" /></div> <br>
                 <div class="line submit"><input type="submit" value="Submit" /></div>
  
                 <p>Note: Please make sure your details are correct before submitting form and that all fields marked with * are completed!.</p>
