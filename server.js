541201dd99d20763ed438dc607e4f69e863dc4e2
mushahidabbasi <zebhussain2013@gmail.com>
2016-11-21 09:33:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,28 +28,7 @@ var articles={
     </li>
     <p>`
     },
-    'Register':{title:'Register',
-    content:
-    `
-<div id ="container">
-<form>
-                <h1>Registration Page</h1>
-                <div class="line"><label for="username">Username *: </label><input type="text" id="username" /></div> <br>
-                <div class="line"><label for="pwd">Password *: </label><input type="password" id="pwd" /></div> <br>
-                
-                <div class="line"><label for="surname">Surname *: </label><input type="text" id="surname" /></div> <br>
-                <div class="line"><label for="other_names">Other Names *: </label><input type="text" id="names" /></div> <br>
-                <div class="line"><label for="dob">Date of Birth *: </label><input type="text" id="dob" /></div> <br>
-                <div class="line"><label for="email">Email *: </label><input type="email" id="email" /></div> <br>
-               
-                <div class="line"><label for="tel">Telephone: </label><input type="text" id="tel" /></div> <br>
-                <div class="line"><label for="add">Address *: </label><input type="text" id="add" /></div> <br>
-                <div class="line"><label for="ptc">Post Code *: </label><input type="text" id="ptc" /></div> <br>
-                <div class="line submit"><input type="submit" value="Submit" /></div>
- 
-                <p>Note: Please make sure your details are correct before submitting form and that all fields marked with * are completed!.</p>
-            </form> `
-    },
+    
     'Login':{title:'Login',
     content:`
     <form>
