ca325ac94b8e0b192c7cf50a51d8bd6c17108309
mushahidabbasi <zebhussain2013@gmail.com>
2016-11-21 15:33:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+var crypto = require('crypto');
+var bodyParser= require('body-parser');
+
+var config={
+    username:'mujahirabbasi',
+    database:'mujahirabbasi',
+    host:'db.imad.hasura.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+    };
+
+
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
+
+
+var pool = new Pool(config);
+app.get('/test-db',function(req,res){
+    pool.query('SELECT * FROM test', function(err, result) {
+      
+      if(err) {
+          res.status(500).send(err.toString());
+      }
+    else
+    {
+        res.send(JSON.stringify(result.rows));
+    }
+    });
+    });
+
+
 
 var articles={
     'About':{ title:'About',
