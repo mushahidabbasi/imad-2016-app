af93cd26cd945ccc2ded788f93954bb4c9a0355c
mushahidabbasi <zebhussain2013@gmail.com>
2016-11-21 15:38:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -116,7 +116,68 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+function hash(input,salt){
+    var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return ["pbkdf2","10000",salt,hashed.toString('hex')].join('$');
+}
+app.get('/hash/:input',function(req,res){
+    var hashedString=hash(req.params.input,'this-is-a-random-string');
+    res.send(hashedString);
+});
 
+app.post('/create-user',function(req,res){
+    console.log("post");
+      var username=req.body.username;
+       var password=req.body.password;
+    var title='Test';
+var date='11/11/2016';
+var heading='Test';
+var content='Test Content';
+   var htmlTemplate=`<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+         <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+        <body>
+        <div class =container>   
+            <div>
+            <a href="/">Home</a>
+            </div>
+            <h3>
+                ${heading}
+            </h3>
+            <div>
+                ${password}
+            </div>
+            <hr/>
+            <div>
+            ${username}  
+            </div>
+        </div>    
+        </body>
+</html>
+`;
+res.send( htmlTemplate);
+     var username=req.body.username;
+     var password=req.body.password;
+     var salt=crypto.randomBytes(128).toString('hex');
+    var dbString = hash(password,salt);
+ //    res.send('User succesfully created'+username);
+    pool.query('INSERT INTO "user"(username,password)VALUES($1,$2)',[username,dbString],function(err,result) {
+        
+        if(err) {
+          res.status(500).send(err.toString());
+      }
+    else
+    {
+        res.send('User succesfully created'+username);
+    }
+    
+    });
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
